a1ad78da2da8d88f25e48651227b17801c3a8f3b
Jean Carlos <alinecaleffi97@gmail.com>
2022-04-20 12:06:08 -0300

    alterando arquivos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ListarComponent } from './professores/listar/listar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: AppComponent},
+  {path: 'professores', component: ListarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
