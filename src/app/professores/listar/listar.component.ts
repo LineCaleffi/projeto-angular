404e703070b5c600fe0b051f9599a718e497cf73
Jean Carlos <alinecaleffi97@gmail.com>
2022-04-20 14:07:48 -0300

    atualizando

--- src/app/professores/listar/listar.component.ts
+++ src/app/professores/listar/listar.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-listar',
@@ -9,15 +10,21 @@ export class ListarComponent implements OnInit {
 
   @Input()
   empresaFilho : string = '';
-
   professores = [
-    {id : 1, nome:"Aline", email:"aline.caleffi@nava.com.br"},
-    {id : 2, nome:"Lola", email:"lola@nava.com.br"}
-  ]
-
-  constructor() { }
-
-  ngOnInit(): void {
+    { id : 1, nome: "Fabrizio", email: "fabrizio@grandeporte.com.br" },
+    { id : 2, nome: "Nelson", email: "nelson@grandeporte.com.br" }
+  ];
+  //private activatedRoute : ActivatedRoute
+   // modificador de acesso, nome da variável e Classe do objeto a ser injetado
+  constructor(private activatedRoute: ActivatedRoute) {
+   // this.activatedRoute = new ActivatedRoute();
   }
 
+  ngOnInit(): void {
+     this.activatedRoute.params.subscribe(
+       (data) => {
+         console.log(data);
+         }
+         );
+  }
 }
