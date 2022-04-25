b195250912dcbdb9b418a0058a42087c336193db
Aline <alinecaleffi97@gmail.com>
2022-04-25 13:11:17 -0300

    atualizando

--- src/app/professores/listar/listar.component.ts
+++ src/app/professores/listar/listar.component.ts
@@ -12,7 +12,10 @@ export class ListarComponent implements OnInit {
   empresaFilho : string = '';
   professores = [
     { id : 1, nome: "Fabrizio", email: "fabrizio@grandeporte.com.br" },
-    { id : 2, nome: "Nelson", email: "nelson@grandeporte.com.br" }
+    { id : 2, nome: "Nelson", email: "nelson@grandeporte.com.br" },
+    { id : 3, nome: "Marcelo", email: "marcelo@grandeporte.com.br" },
+    { id : 4, nome: "Ivan", email: "ivan@grandeporte.com.br" },
+    { id : 5, nome: "Gabriela", email: "gabriela@grandeporte.com.br" }
   ];
   //private activatedRoute : ActivatedRoute
    // modificador de acesso, nome da variável e Classe do objeto a ser injetado
