3cd619f54d628db954a25d0e0dce9a7721b9ae88
Jean Carlos <alinecaleffi97@gmail.com>
2022-04-25 12:35:12 -0300

    2504-atualizado

--- src/app/formularios/mostrar-mensagem/mostrar-mensagem.component.ts
+++ src/app/formularios/mostrar-mensagem/mostrar-mensagem.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-mostrar-mensagem',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mostrar-mensagem.component.scss']
 })
 export class MostrarMensagemComponent implements OnInit {
+
+  @Input()
+  controles : any;
 
   constructor() { }
 
