71519a8622d67eebde1b79d6dc28dc541b7cd3dc
Jean Carlos <alinecaleffi97@gmail.com>
2022-04-25 10:39:28 -0300

    2504-atualizando

--- src/app/formularios/reativo-form/reativo-form.component.ts
+++ src/app/formularios/reativo-form/reativo-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reativo-form',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReativoFormComponent implements OnInit {
 
-  constructor() { }
+  // inicializando a váriavle meuForm com um objeto com controles vazios -> {}
+  meuForm: FormGroup = new FormGroup({});
+
+  // FormBuilder é um serviço utilizado para ajudar a criar os mecanismos de controles do formulário
+  constructor(private formBuilder : FormBuilder) { }
 
   ngOnInit(): void {
   }
