9f6328917d90a393d76349f0196c4df31235718f
Aline <alinecaleffi97@gmail.com>
2022-04-26 16:44:14 -0300

    ultima-atualizacao

--- src/app/professores-gp/form/form.component.ts
+++ src/app/professores-gp/form/form.component.ts
@@ -9,11 +9,14 @@ import { ProfessoresGpService } from '../professores-gp.service';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
+  // 1-) alocar a variável que vai ter os controles do form
   meuForm : FormGroup = new FormGroup({});
 
+  // 2-) injetar o FormBuilder para poder ajudar a criar os controles do form
   constructor(private formBuilder : FormBuilder, private professorGP : ProfessoresGpService,
               private router : Router) { }
 
+              // 3-) alocar os controles na variável meuForm através do formBuilder
   ngOnInit(): void {
     this.meuForm=this.formBuilder.group({
       nome:[null,[Validators.required]],
@@ -21,10 +24,13 @@ export class FormComponent implements OnInit {
     });
   }
 
+  // 4-) integrar os controles do form no HTML
+  // Salva novos dados no BD
   onSubmit1(){
     console.log(this.meuForm.value);
     this.professorGP.save(this.meuForm.value).subscribe((data)=>{
       console.log(data);
+      // o navigate é para redirecionar para uma outra rota de interesse após salva
       this.router.navigate(['/professores-gp']);
     });
   }
--- src/app/professores-gp/listar-gp/listar-gp.component.ts
+++ src/app/professores-gp/listar-gp/listar-gp.component.ts
@@ -9,9 +9,12 @@ import { ProfessoresGpService } from '../professores-gp.service';
 })
 export class ListarGPComponent implements OnInit {
 
+  // CRIA UM ARRAY VAZIO
   professores : any = [];
+
   constructor(private activatedRoute : ActivatedRoute, private professorGP : ProfessoresGpService) { }
 
+  // PEGA OS DADOS DO BANCO DE DADOS
   ngOnInit(): void {
     this.professorGP.getAll().subscribe((data)=>{
       console.log(data);
