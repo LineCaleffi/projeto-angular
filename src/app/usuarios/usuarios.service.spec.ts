f6013dc3261a02a312cd140f71cf17eeda43dfaf
Aline <alinecaleffi97@gmail.com>
2022-04-29 11:21:34 -0300

    2904-atualizado

--- src/app/usuarios/usuarios.service.spec.ts
+++ src/app/usuarios/usuarios.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { UsuaripsService } from './usuarios.service';
+import { UsuariosService } from './usuarios.service';
 
 describe('UsuaripsService', () => {
-  let service: UsuaripsService;
+  let service: UsuariosService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(UsuaripsService);
+    service = TestBed.inject(UsuariosService);
   });
 
   it('should be created', () => {
